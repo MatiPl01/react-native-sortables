20b5d9d32e396d6f62b31e5b3f4daad6709d5e07
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-06 18:32:39 +0200

    Add formatted files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  "extends": "react-native-matipl01",
-}
+  extends: 'react-native-matipl01'
+};
--- example/bare/babel.config.cjs
+++ example/bare/babel.config.cjs
@@ -23,7 +23,7 @@ module.exports = function (api) {
             '@': path.join(appDir, 'src')
           }
         }
-      ],
+      ]
     ]
   };
 };
