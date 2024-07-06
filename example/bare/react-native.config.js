d1e69a53c76388259c53cec46eedd43f2ecc0bea
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-06 16:08:42 +0200

    Fix type issues

--- example/bare/react-native.config.js
+++ example/bare/react-native.config.js
@@ -11,6 +11,9 @@ const appPkg = require(path.resolve(appDir, 'package.json'));
 
 let dependencies = {};
 
+console.log('rootPkg', rootPkg);
+console.log('appPkg', appPkg);
+
 [
   // Include library dependencies that will normally be installed with
   // the library (dependencies in the top-level package.json)
@@ -24,5 +27,7 @@ let dependencies = {};
     root: path.resolve(__dirname, `../../node_modules/${dep}`)
   };
 });
+
+console.log('dependencies', dependencies);
 
 module.exports = { dependencies };
