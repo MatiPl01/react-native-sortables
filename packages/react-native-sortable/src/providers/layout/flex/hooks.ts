a6629adc63e7c336f2014002baee4a5e9453009f
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-12-08 01:40:22 +0100

    Uncomment flex order updater

--- packages/react-native-sortable/src/providers/layout/flex/hooks.ts
+++ packages/react-native-sortable/src/providers/layout/flex/hooks.ts
@@ -1,9 +1,13 @@
 import type { Coordinate, Dimension } from '../../../types';
-import { useOrderUpdater } from '../../shared';
+import { reorderItems } from '../../../utils';
+import { useCommonValuesContext, useOrderUpdater } from '../../shared';
 import { useFlexLayoutContext } from './FlexLayoutProvider';
 
 export function useFlexOrderUpdater(): void {
-  const { flexDirection } = useFlexLayoutContext();
+  const { indexToKey, itemDimensions, itemPositions, keyToIndex } =
+    useCommonValuesContext();
+  const { crossAxisGroupOffsets, flexDirection, itemGroups, keyToGroup } =
+    useFlexLayoutContext();
 
   let mainCoordinate: Coordinate = 'x';
   let crossCoordinate: Coordinate = 'y';
@@ -17,91 +21,92 @@ export function useFlexOrderUpdater(): void {
     crossDimension = 'width';
   }
 
-  useOrderUpdater(() => {
-    'worklet';
-    // TODO - reimplement
-    return undefined;
-    // let groupIndex = keyToGroup.value[activeKey];
-    // if (groupIndex === undefined) {
-    //   return;
-    // }
+  useOrderUpdater(
+    ({ activeIndex, activeKey, position, strategy, touchPosition }) => {
+      'worklet';
+      let groupIndex = keyToGroup.value[activeKey];
+      if (groupIndex === undefined) {
+        return;
+      }
 
-    // // Select the group in which the active item is currently located
-    // let offsetBefore = crossAxisGroupOffsets.value[groupIndex];
-    // while (
-    //   offsetBefore !== undefined &&
-    //   groupIndex >= 0 &&
-    //   touchPosition[crossCoordinate] < offsetBefore
-    // ) {
-    //   groupIndex -= 1;
-    //   offsetBefore = crossAxisGroupOffsets.value[groupIndex];
-    // }
+      // Select the group in which the active item is currently located
+      let offsetBefore = crossAxisGroupOffsets.value[groupIndex];
+      while (
+        offsetBefore !== undefined &&
+        groupIndex >= 0 &&
+        touchPosition[crossCoordinate] < offsetBefore
+      ) {
+        groupIndex -= 1;
+        offsetBefore = crossAxisGroupOffsets.value[groupIndex];
+      }
 
-    // let offsetAfter = crossAxisGroupOffsets.value[groupIndex + 1];
-    // while (
-    //   offsetAfter !== undefined &&
-    //   groupIndex < itemGroups.value.length &&
-    //   touchPosition[crossCoordinate] > offsetAfter
-    // ) {
-    //   groupIndex += 1;
-    //   offsetAfter = crossAxisGroupOffsets.value[groupIndex + 1];
-    // }
+      let offsetAfter = crossAxisGroupOffsets.value[groupIndex + 1];
+      while (
+        offsetAfter !== undefined &&
+        groupIndex < itemGroups.value.length &&
+        touchPosition[crossCoordinate] > offsetAfter
+      ) {
+        groupIndex += 1;
+        offsetAfter = crossAxisGroupOffsets.value[groupIndex + 1];
+      }
 
-    // // Check if the active item center is overlapping with another item
-    // // within the same group
-    // let overlappingItemKey: string | undefined;
-    // const group = itemGroups.value[groupIndex];
-    // if (!group) {
-    //   return;
-    // }
-    // for (const key of group) {
-    //   if (key === activeKey) {
-    //     continue;
-    //   }
+      // Check if the active item center is overlapping with another item
+      // within the same group
+      let overlappingItemKey: string | undefined;
+      const group = itemGroups.value[groupIndex];
+      if (!group) {
+        return;
+      }
+      for (const key of group) {
+        if (key === activeKey) {
+          continue;
+        }
 
-    //   const otherDimensions = itemDimensions.value[key];
-    //   if (!otherDimensions) {
-    //     continue;
-    //   }
-    //   const otherPosition = itemPositions.value[key];
-    //   if (!otherPosition) {
-    //     continue;
-    //   }
+        const otherDimensions = itemDimensions.value[key];
+        if (!otherDimensions) {
+          continue;
+        }
+        const otherPosition = itemPositions.value[key];
+        if (!otherPosition) {
+          continue;
+        }
 
-    //   // Item before the active item in the group
-    //   if (otherPosition[mainCoordinate] < position[mainCoordinate]) {
-    //     const otherEnd =
-    //       otherPosition[mainCoordinate] + otherDimensions[mainDimension];
-    //     if (otherEnd > touchPosition[mainCoordinate]) {
-    //       overlappingItemKey = key;
-    //       break;
-    //     }
-    //   }
+        // Item before the active item in the group
+        if (otherPosition[mainCoordinate] < position[mainCoordinate]) {
+          const otherEnd =
+            otherPosition[mainCoordinate] + otherDimensions[mainDimension];
+          if (otherEnd > touchPosition[mainCoordinate]) {
+            overlappingItemKey = key;
+            break;
+          }
+        }
 
-    //   // Item after the active item in the group
-    //   if (otherPosition[mainCoordinate] > position[mainCoordinate]) {
-    //     const otherStart = otherPosition[mainCoordinate];
-    //     if (otherStart < touchPosition[mainCoordinate]) {
-    //       overlappingItemKey = key;
-    //       break;
-    //     }
-    //   }
-    // }
+        // Item after the active item in the group
+        if (otherPosition[mainCoordinate] > position[mainCoordinate]) {
+          const otherStart = otherPosition[mainCoordinate];
+          if (otherStart < touchPosition[mainCoordinate]) {
+            overlappingItemKey = key;
+            break;
+          }
+        }
+      }
 
-    // if (overlappingItemKey === undefined) {
-    //   return;
-    // }
-    // const overlappingIndex = keyToIndex.value[overlappingItemKey];
-    // if (overlappingIndex === undefined) {
-    //   return;
-    // }
+      if (overlappingItemKey === undefined) {
+        return;
+      }
+      const overlappingIndex = keyToIndex.value[overlappingItemKey];
+      if (overlappingIndex === undefined) {
+        return;
+      }
 
-    // // Return the new order of items
-    // return reorderItems(
-    //   indexToKey.value,
-    //   activeIndex,
-    //   overlappingIndex,
-    //   strategy
-    // );
-  }, [mainCoordinate, crossCoordinate, mainDimension, crossDimension]);
+      // Return the new order of items
+      return reorderItems(
+        indexToKey.value,
+        activeIndex,
+        overlappingIndex,
+        strategy
+      );
+    },
+    [mainCoordinate, crossCoordinate, mainDimension, crossDimension]
+  );
 }
