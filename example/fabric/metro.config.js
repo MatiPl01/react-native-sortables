54200ccda88132cfc33140d467fcf3961e419e81
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-02 22:06:06 +0100

    Fix android setup

--- example/fabric/metro.config.js
+++ example/fabric/metro.config.js
@@ -12,7 +12,12 @@ const workspaces = getWorkspaces(__dirname).filter(
 );
 
 const customConfig = {
-  watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces]
+  watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces],
+  server: {
+    // This field breaks app entry point resolution in the default expo
+    // metro config, hence we set it to null
+    unstable_serverRoot: null
+  }
 };
 
 module.exports = wrapWithReanimatedMetroConfig(
