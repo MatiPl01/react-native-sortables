0ebcfd803cc4c3ad7a9a13da07a5edad7e4816cd
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-18 21:04:52 +0100

    chore: Add warning for debug and scrollableRef prop changes, adjust debug examples (#187)
    ## Description
    
    `debug` and `scrollableRef` shouldn't be changed on the fly as they
    add/remove providers in the component tree, which causes the entire
    sortable component render. Because of that, the current state of the
    component is lost.
    
    To make it obvious for the user that they shouldn't modify these props
    on the fly, after the component is rendered for the first time, I added
    a warning for these props changes.

--- packages/react-native-sortable/src/hooks/callbacks/useStableCallback.ts
+++ packages/react-native-sortable/src/hooks/callbacks/useStableCallback.ts
@@ -1,10 +1,13 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import type { AnyFunction } from '../../types';
 
 export default function useStableCallback<C extends AnyFunction>(callback: C) {
   const callbackRef = useRef(callback);
-  callbackRef.current = callback;
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
 
   return useCallback((...args: Parameters<C>) => {
     callbackRef.current(...args);
--- packages/react-native-sortable/src/hooks/index.ts
+++ packages/react-native-sortable/src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './callbacks';
 export * from './handlers';
 export * from './reanimated';
 export { default as useHaptics } from './useHaptics';
+export { default as useWarnOnPropChange } from './useWarnOnPropChange';
