707678875d26d11735750f99f49d06c5dc6d5f25
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-19 19:11:42 +0100

    docs: Prepare first version of README (#188)
    ## Description
    
    This PR adds first version of readme and some missing information in the
    `package.json` file.

--- example/app/src/examples/SortableGrid/DataChangeExample.tsx
+++ example/app/src/examples/SortableGrid/DataChangeExample.tsx
@@ -72,7 +72,9 @@ export default function DataChangeExample() {
   }, []);
 
   const sortItems = useCallback(() => {
-    setData(prevData => [...prevData].sort());
+    setData(prevData =>
+      [...prevData].sort((a, b) => +a.split(' ')[1]! - +b.split(' ')[1]!)
+    );
   }, []);
 
   const onRemoveItem = useCallback((item: string) => {
