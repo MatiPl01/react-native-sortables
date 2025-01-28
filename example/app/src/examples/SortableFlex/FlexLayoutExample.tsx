e83e53aff92dd0e0cfbb7d374514a3dfe93a5a29
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-01-28 22:59:19 +0100

    fix: Fix flex ordering for flex with alignment, prepare for release (#206)
    ## Description
    
    This PR:
    - Fixes invalid flex ordering when alignment (like `space-between`) is
    used, when the gap between items is greater than the gap set by the
    user,
    - Updates default `autoScrollActivationOffset` value from `40` to `75`,
    - Adds `Sortable.Layer` component needed on Paper to examples,
    - Updates Pods on Paper example

--- example/app/src/examples/SortableFlex/FlexLayoutExample.tsx
+++ example/app/src/examples/SortableFlex/FlexLayoutExample.tsx
@@ -123,7 +123,7 @@ export default function FlexLayoutExample() {
               bordered>
               <Text style={styles.groupTitle}>{direction}</Text>
               <View style={styles.flexWrapper}>
-                <Sortable.Flex flexDirection={direction} {...flexStyle} debug>
+                <Sortable.Flex flexDirection={direction} {...flexStyle}>
                   {DATA.map((item, index) => (
                     <FlexCell
                       height={34 + (index % 3) * 10}
