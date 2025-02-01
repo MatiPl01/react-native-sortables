c81a01865c79db3a70228233f70601eca3e9578d
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-02-01 13:16:04 +0100

    Fix some flex reordering issues

--- example/app/src/examples/SortableFlex/PlaygroundExample.tsx
+++ example/app/src/examples/SortableFlex/PlaygroundExample.tsx
@@ -1,34 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import Sortable from 'react-native-sortables';
 
-import { ScrollScreen } from '@/components';
 import { colors, text } from '@/theme';
-import { getCategories, IS_WEB } from '@/utils';
 
-const DATA = getCategories(IS_WEB ? 30 : 10);
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const PADDING = 20;
+const GAP = 15;
+const COLUMN_WIDTH = (WINDOW_WIDTH - 2 * PADDING - GAP) / 2;
 
 export default function Flex() {
   return (
-    <ScrollScreen includeNavBarHeight>
-      <Sortable.Flex gap={10} padding={10}>
-        {/* You can render anything within the Sortable.Flex component */}
-        {DATA.map(item => (
-          <View key={item} style={styles.cell}>
-            <Text style={styles.text}>{item}</Text>
-          </View>
-        ))}
-      </Sortable.Flex>
-    </ScrollScreen>
+    <Sortable.Flex
+      justifyContent='space-between'
+      rowGap={GAP}
+      padding={PADDING}
+      debug>
+      <SingleColumnTile />
+      <SingleColumnTile />
+      <TwoColumnsTile />
+      <TwoColumnsTile />
+    </Sortable.Flex>
   );
 }
 
+function SingleColumnTile() {
+  return <View style={[styles.tile, styles.singleColumnTile]} />;
+}
+
+function TwoColumnsTile() {
+  return <View style={[styles.tile, styles.twoColumnsTile]} />;
+}
+
 const styles = StyleSheet.create({
-  cell: {
+  tile: {
     alignItems: 'center',
     backgroundColor: '#36877F',
-    borderRadius: 9999,
+    borderRadius: 10,
     justifyContent: 'center',
-    padding: 10
+    height: COLUMN_WIDTH
+  },
+  singleColumnTile: {
+    width: COLUMN_WIDTH
+  },
+  twoColumnsTile: {
+    height: COLUMN_WIDTH / 2,
+    width: 2 * COLUMN_WIDTH + GAP
   },
   text: {
     ...text.label2,
