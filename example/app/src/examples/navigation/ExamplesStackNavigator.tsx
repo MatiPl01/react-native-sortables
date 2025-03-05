dcd70eda6971c9fc721eff6f68bb8e9d9c34a8e7
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-03-05 02:26:33 +0100

    Restore nav header visibility

--- example/app/src/examples/navigation/ExamplesStackNavigator.tsx
+++ example/app/src/examples/navigation/ExamplesStackNavigator.tsx
@@ -67,7 +67,6 @@ function createStackNavigator(routes: Routes): React.ComponentType {
           <StackNavigator.Navigator
             screenOptions={{
               headerLeft: () => <BackButton />,
-              headerShown: false,
               headerTitleAlign: 'center'
             }}>
             {createNavigationScreens(routes, 'Examples', ['Examples'])}
