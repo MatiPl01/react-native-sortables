656c722f0cfaa4c71dfc5e6fe5a8e07a6a9782dd
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-05-26 17:31:08 +0200

    Remove useUICallback hook

--- packages/react-native-sortables/src/hooks/reanimated/index.ts
+++ packages/react-native-sortables/src/hooks/reanimated/index.ts
@@ -1,4 +1,3 @@
 export { default as useAnimatableValue } from './useAnimatableValue';
 export { default as useStableCallbackValue } from './useStableCallbackValue';
-export { default as useUICallback } from './useUICallback';
 export { default as useUIStableCallback } from './useUIStableCallback';
