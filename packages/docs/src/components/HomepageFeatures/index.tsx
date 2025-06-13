428dda39f0c19593e886c3b6bed992f1a6e681e6
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-13 16:20:03 +0200

    Update eslint in docs package

--- packages/docs/src/components/HomepageFeatures/index.tsx
+++ packages/docs/src/components/HomepageFeatures/index.tsx
@@ -15,38 +15,38 @@ type FeatureItem = {
 
 const FeatureList: Array<FeatureItem> = [
   {
-    Svg: mountainSvg,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
     ),
+    Svg: mountainSvg,
     title: 'Easy to Use'
   },
   {
-    Svg: treeSvg,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
     ),
+    Svg: treeSvg,
     title: 'Focus on What Matters'
   },
   {
-    Svg: reactSvg,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
     ),
+    Svg: reactSvg,
     title: 'Powered by React'
   }
 ];
 
-function Feature({ Svg, description, title }: FeatureItem) {
+function Feature({ description, Svg, title }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className='text--center'>
