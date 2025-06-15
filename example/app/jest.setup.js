97722c5d7d63caaba3aaa269fe40b950ac4f1968
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-15 15:19:23 +0200

    Add missing jest mock

--- example/app/jest.setup.js
+++ example/app/jest.setup.js
@@ -1,5 +1,12 @@
 import 'react-native-gesture-handler/jestSetup';
 
+// Adds missing mock in RN 0.80.0
+// https://github.com/facebook/react-native/issues/51993#issuecomment-2970614900
+jest.mock('react-native/Libraries/Components/RefreshControl/RefreshControl', () => ({
+  __esModule: true,
+  default: require('./__mocks__/RefreshControlMock'),
+}));
+
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
