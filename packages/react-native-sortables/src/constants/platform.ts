26932348587b932d4d4848ec346645483771d6c3
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-26 09:08:37 +0200

    fix: Invalid ScrollView position on input focus (#431)
    ## Description
    
    ### Issue description
    
    This PR fixes invalid `ScrollView` behavior when the `TextInput`
    component was focused. I noticed that this is caused by the fact that
    all items rendered within the sortable component use transforms for
    positioning (as transforms are said to be faster than `top`/`left`
    props).
    
    Unfortunately, the position of the `ScrollView` is adjusted based on the
    render position of the `TextInput` component but in our case all items
    are rendered in the top left corner of the sortable component container
    and then transformed to their proper positions on the screen.
    
    ### Solution
    
    I decided to replace transforms with `top` and `left` layout props which
    seem to work fine.
    
    ## Test example
    
    | Before | After |
    |-|-|
    | <video
    src="https://github.com/user-attachments/assets/83168a03-3216-43d2-b49b-b603b5e332c0"
    /> | <video
    src="https://github.com/user-attachments/assets/d6c3ce3f-3f90-4cf2-95a2-cc153b0d4fe8"
    /> |
    
    <details>
    <summary>Source code</summary>
    
    ```tsx
    import { useCallback } from 'react';
    import { TextInput, View } from 'react-native';
    import Animated, { useAnimatedRef } from 'react-native-reanimated';
    import Sortable, { type SortableGridRenderItem } from 'react-native-sortables';
    
    const testData = new Array(50).fill(0).map((_, i) => ({
      id: i,
      name: `Exercise ${i}`
    }));
    
    export default function PlaygroundExample() {
      const scrollableRef = useAnimatedRef<Animated.ScrollView>();
    
      const renderExerciseItem = useCallback<
        SortableGridRenderItem<(typeof testData)[0]>
      >(
        ({ item }) => (
          <View style={{ backgroundColor: '#ef4444', height: 160 }}>
            <TextInput
              keyboardType='numeric'
              placeholder={`TextInput#${item.id}`}
            />
          </View>
        ),
        []
      );
    
      return (
        <Animated.ScrollView
          contentContainerStyle={{ padding: 16, paddingBottom: 128, gap: 16 }}
          keyboardShouldPersistTaps='handled'
          ref={scrollableRef}>
          <Sortable.Grid
            activeItemScale={1.02}
            columnGap={40}
            columns={1}
            data={testData}
            enableActiveItemSnap={false}
            overDrag='vertical'
            renderItem={renderExerciseItem}
            rowGap={10}
            scrollableRef={scrollableRef}
          />
        </Animated.ScrollView>
      );
    }
    ```
    </details>

--- packages/react-native-sortables/src/constants/platform.ts
+++ packages/react-native-sortables/src/constants/platform.ts
@@ -4,9 +4,3 @@ import { Platform } from 'react-native';
 export const IS_WEB = Platform.OS === 'web';
 
 export const IS_REACT_19 = reactVersion.startsWith('19.');
-
-export function isPaper() {
-  // @ts-expect-error - globalThis is not typed
-  // eslint-disable-next-line no-underscore-dangle
-  return !globalThis?._IS_FABRIC && !IS_WEB;
-}
