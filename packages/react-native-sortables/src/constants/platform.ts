be78141c3a58e7ad2ec3a31b8313efa571060f37
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-27 23:46:53 +0200

    perf: More performant items reordering (#435)
    ## Description
    
    This PR implements more performant items reordering in sortable
    components that takes architectural differences into account (the
    `onLayout` callback is called more often on the New Architecture if we
    use layout-based items positioning).

--- packages/react-native-sortables/src/constants/platform.ts
+++ packages/react-native-sortables/src/constants/platform.ts
@@ -4,3 +4,9 @@ import { Platform } from 'react-native';
 export const IS_WEB = Platform.OS === 'web';
 
 export const IS_REACT_19 = reactVersion.startsWith('19.');
+
+export function isFabric() {
+  // @ts-expect-error - global is not typed
+  // eslint-disable-next-line no-underscore-dangle
+  return !!(globalThis?._IS_FABRIC ?? global?._IS_FABRIC);
+}
