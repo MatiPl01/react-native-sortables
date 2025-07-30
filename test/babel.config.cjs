b43a5f78e7a130d2f973daa0ad19777f88ebaf19
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-07-30 19:34:57 +0200

    Patch reanimated 3.19.0

--- test/babel.config.cjs
+++ test/babel.config.cjs
@@ -3,5 +3,8 @@
  */
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['@babel/plugin-transform-export-namespace-from', 'react-native-worklets/plugin']
+  plugins: [
+    '@babel/plugin-transform-export-namespace-from',
+    'react-native-worklets/plugin'
+  ]
 };
