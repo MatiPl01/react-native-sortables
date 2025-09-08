e11ca25755c81ccbcdd975714a3fe40e0dbc6bd6
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-08 13:29:08 +0200

    Fix invalid builder bob config

--- packages/react-native-sortables/bob.config.cjs
+++ packages/react-native-sortables/bob.config.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   source: 'src',
   output: 'dist',
-  exclude: [
-    '**/{__tests__,__fixtures__,__mocks__}/**',
-    '**/*.test.{js,jsx,ts,tsx}'
-  ],
+  exclude: '{**/{__tests__,__fixtures__,__mocks__}/**,**/*.test.{js,jsx,ts,tsx}}',
   targets: [
     ['module', { configFile: true }],
     ['typescript', { project: 'tsconfig.build.json' }]
