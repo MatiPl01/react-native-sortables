830a156f7a5ddfa985804366e9ceec49d26321e6
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-09 01:45:58 +0200

    Fix auto scroll getting stuck sometimes

--- example/app/src/examples/SortableGrid/features/DragHandleExample.tsx
+++ example/app/src/examples/SortableGrid/features/DragHandleExample.tsx
@@ -80,6 +80,7 @@ export default function DragHandleExample() {
         style={flex.fill}>
         <Sortable.Grid
           activeItemScale={1}
+          autoScrollMaxOverscroll={[50, 120]}
           columnGap={10}
           columns={columns}
           data={DATA}
@@ -91,6 +92,7 @@ export default function DragHandleExample() {
           rowGap={10}
           scrollableRef={scrollableRef}
           customHandle
+          debug
         />
         <Spacer height={bottomNavBarHeight} />
       </Animated.ScrollView>
