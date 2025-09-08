5ee2e9828f2bb31938f86b82e5827b7790860495
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-09-08 14:50:06 +0200

    Fix formatting

--- packages/react-native-sortables/bob.config.cjs
+++ packages/react-native-sortables/bob.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   source: 'src',
   output: 'dist',
-  exclude: '{**/{__tests__,__fixtures__,__mocks__}/**,**/*.test.{js,jsx,ts,tsx}}',
+  exclude:
+    '{**/{__tests__,__fixtures__,__mocks__}/**,**/*.test.{js,jsx,ts,tsx}}',
   targets: [
     ['module', { configFile: true }],
     ['typescript', { project: 'tsconfig.build.json' }]
